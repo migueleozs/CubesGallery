a59e06f55ecfa441fa88abdcea33f067a843351f
migueleozs <migueleo985@gmail.com>
2025-01-06 15:44:43 -0300

    Initial Commit

---
+++ Gallery/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Gallery
+//
+//  Created by Miguel Zamudio on 06/01/2025.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ GalleryTests/GalleryTests.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryTests.swift
+//  GalleryTests
+//
+//  Created by Miguel Zamudio on 06/01/2025.
+//
+
+import Testing
+@testable import Gallery
+
+struct GalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
